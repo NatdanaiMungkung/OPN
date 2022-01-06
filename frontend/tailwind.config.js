90ddcc756c144c6bd61b73b7cc1307baf4aaa9f0
norule <iwall.admin@gmail.com>
2022-01-06 21:34:15 +0700

    WIP

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#5882E3',
+      'light-blue': '#254870',
+      'yellow': '#ffc107',
+      'red': '#dc3545',
+      'white': '#fff',
+      'background': '#012d5e'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Roboto Slab', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
